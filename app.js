68c56dba4373c9ae8e2ea6b4e18b5b7338732603
pranavp14 <pranavpatil.6767@gmail.com>
2022-01-25 20:18:16 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
-require("./db/conn");
+// require("./db/conn");
 
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
 const path = require('path');
 const hbs = require('hbs');
+const bodyParser = require("body-parser");
 
 dotenv.config({ path: './env'});
 
@@ -14,16 +15,16 @@ const port = process.env.PORT || 5001;
 const publicDirectory = path.join(__dirname,'./public');
 app.use(express.static(publicDirectory));
 
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.set('view engine','hbs');
 
-app.use('/auth',require('./routes/auth'));
 
 //Define routes
 
 app.use('/',require('./routes/pages'));
+app.use('/auth',require('./routes/auth'));
 
 app.listen(port, ()=>{
     console.log("server is running");
