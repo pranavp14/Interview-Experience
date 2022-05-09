6336d7b2a5801d1ad3a75fabe8c1c152452afd24
pranavp14 <pranavpatil.6767@gmail.com>
2022-05-09 21:08:59 +0530

    changes in mobile view

--- db/conn.js
+++ db/conn.js
@@ -10,7 +10,7 @@ const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: '',
-    database: 'nodejs-login',
+    database:  'interview',
     multipleStatements: true
 });
 
